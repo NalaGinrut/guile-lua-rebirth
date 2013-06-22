ac3184ef2c742b4cdae37a19c38ec4e0c9019b0b
Nala Ginrut <nalaginrut@gmail.com>
2013-06-22 10:21:27 +0800

    add Lua LALR parser

--- lua/parser.scm
+++ lua/parser.scm
@@ -16,7 +16,6 @@
 (define-module (language lua parser)
   #:use-module (language lua utils)
   #:use-module (language lua lexer)
-  #:use-module (language lua runtime)
   #:export (make-parser read-lua))
 
 (define (read-lua port)
@@ -33,6 +32,7 @@
    (left: multi div mod)
    (right: not hash)
    (right: expt)
+   (right: assign hash)
 
    ;; punctuations
    semi-colon comma dot lbrace rbrace lparen rparen lbracket rbracket
@@ -45,83 +45,93 @@
    ;; misc
    id sp-id)
 
-  (program: (block) : $1
+  (program: (chunk) : $1
             (*eoi*) : *eof-object*)
   
-  (terminator (semi-colon) : $1
-              () : '(begin))
+  (chunk (block) : $1)
 
-  (block (scope stmt-list) : $x
-         (scope stmt-list last-stmt terminator) : $x)
+  (terminator (semi-colon) : $1)
 
-  (ublock (block until exp) : $x)
+  (block () : '(begin) 
+         (stmt-list) : $1)
 
-  (scope () : '(begin)
-         (scope stmt-list binding terminator) : $x)
+  (stmt-list (stmt) : $1
+             (stmt stmt-list) : `(begin ,$1 ,$2)
+             (stmt-list last-stmt terminator) : `(begin ,$1 ,$2))
 
-  (stmt-list () : '(begin)
-             (scope stmt-list stmt terminator) :  $x)
-    
-  (stmt (do block end) : $x
-        (while exp do block end) : $x
-        (repetition do block end) : $x
-        (repeat ublock) : $x
-        (if conds end) : $x
-        (function funcname funcbody) : $x
-        (set-list eq exp-list1) : $x)
+  (last-stmt (break) : '(break)
+             (return) : '(return) ; is it proper to return *unspecified* 
+             (return exp-list1) : `(return ,$1))
 
-  (repetition (for name eq exp-list23) : $x
-              (for name-list in exp-list) : $x)
+  (stmt (do block end) : `(do ,$2)
+        (while exp do block end) : `(while ,$2 do ,$4)
+        (repetition) : $1
+        (repeat block until exp) : `(repeat ,$2 until ,$3)
+        (if-stmt) : $1
+        (function-stmt) : $1
+        (set-list assign exp-list1) : `(global-assign ,$1 ,$3)
+        (local-binding) : $1)
 
-  (conds (cond-list) : $x
-         (cond-list else block) : $x)
+  (repetition (for name assign exp-list23 do block end) 
+              : `(for ,$2 ,$4 do ,$6)
+              (for name-list in exp-list do block end) 
+              : `(for ,$2 in ,$3 do ,$6))
+
+  (if-stmt (if exp then block) : `(if ,$2 then ,$3)
+           (if exp else block) : `(if ,$2 else ,$3)
+           (if exp then block else block) : `(if ,$2 then ,$3 else ,$4)
+           (if exp then block elseif-stmts) : `(if ,$2 then ,$3 ,$4))
   
-  (cond-list (cond) : $x
-             (cond-list elseif cond) : $x)
+  (elseif-stmts (elseif-stmt) : $1
+                (elseif-stmt elseif-stmts) : `(begin ,$1 ,$2))
 
-  (cond (exp then block) : $x)
+  (elseif-stmt (elseif exp then block) : `(if ,$2 then ,$3)
+               (elseif exp then block else block) : `(if ,$2 then ,$3 else ,$4))
 
-  (last-stmt (break) : $x
-             (return) : $x
-             (return exp-list1) : $x)
-
-  (binding (local name-list) : $x
-           (local name-list eq exp-list1) : $x
-           (local function name funcbody) : $x)
+  (local-binding (local name-list) : '(begin) ; nothing to do 
+                 (local name-list assign exp-list1) : `(local-assign ,$3 ,$4)
+                 (local function name funcbody) : `(local function ,$3 ,$4))
 
   (func-name (dotted-name) : $1
-             (dotted-name colon name) : $x)
+             (dotted-name colon name) : `(func-self-pass-in ,$1 ,$3))
 
   (dotted-name (name) : $1
-               (dotted-name dot name) : $x)
+               (dotted-name dot name) : `(func-dot-name ,$1 ,$3))
 
   (name-list (name) : $1
-             (name-list dot name) : $x)
-  
+             (name-list dot name) : `(dot-name ,$1 ,$2))
+
+  (name (id) : $1
+        (sp-id) : $1)
+
   (exp-list1 (exp) : $1
-             (exp-list1 comma exp) : $x)
+             (exp-list1 comma exp) : `(begin ,$1 ,$3))
 
-  (exp-list23 (exp comma exp) : $x
-              (exp comma exp comma exp) : $x)
+  (exp-list23 (exp comma exp) : `(begin ,$1 ,$3)
+              (exp comma exp comma exp) : `(begin ,$1 ,$3 ,$5))
 
-  (exp (nil) : $1
-       (true) : $1
-       (false) : $1
-       (number) : $1
-       (string) : $1
-       (tri-dots) : $1 ; how to deal with var-list?
+  (exp (nil) : 'nil
+       (true) : 'true
+       (false) : 'false ; only nil and false are FALSE
+       (numbers) : $1
+       (string) : `(string ,$1)
+       (tri-dots) : $1
        (func) : $1
        (prefix-exp) : $1
        (table-constructor) : $1
        (not) : $1
        (hash) : $1
        (minus) : $1
-       (exp or exp) : $x
-       (exp and exp) : $x
-       (exp arith-compare exp) : $x
-       (exp concat exp) : $x
-       (exp arith-op exp) : $x)
+       (exp or exp) : `(or ,$1 ,$3)
+       (exp and exp) : `(and ,$1 ,$3)
+       (exp arith-compare exp) : `(,$2 ,$1 ,$3)
+       (exp concat exp) : `(concat ,$1 ,$3)
+       (exp arith-op exp) : `(,$2 ,$1 ,$3))
 
+  (numbers (number) : `(number ,$1)
+           (minus number) : `(- ,$1)
+           (add number) : `(+ ,$1))
+           
   (arith-compare (less-than) : $1
                  (less-eq) : $1
                  (larger-than) : $1
@@ -136,43 +146,47 @@
             (mod) : $1
             (expt) : $1)
 
-  (tri-dots (dot dot dot) : args)
-
   (set-list (var) : $1
-            (set-list comma var) : $x)
+            (set-list comma var) : `(,@$1 ,$3))
 
-  (var (name) : $1
-       (prefix-exp lbracket exp rbracket) : $x
-       (prefix-exp dot name) : $x)
+  (var (name) : `(variable ,$1)
+       (prefix-exp lbracket exp rbracket) : `(table-ref ,$1 ,$2)
+       (prefix-exp dot name) : `(dot-name ,$1 ,$3))
 
-  (function-call (prefix-exp args) : $x
-                 (prefix-exp colon name args) : $x)
+  (prefix-exp (var) : $1
+              (function-call) : $1
+              (lparen exp rparen) : $2)
 
-  (args (lparen rparen) : $x
-        (lparen exp-list1 rparen) : $x
-        (table-constructor) : $1
-        (string) : $1)
+  (function-call (prefix-exp args) : `(function-call ,$1 ,$2)
+                 (prefix-exp colon name args) : `(function-call ,$1
 
-  (func (function funcbody) : $x)
+  (args (lparen rparen) : '(void-args)
+        (lparen exp-list1 rparen) : $2
+        (table-constructor) : `(args-with-table ,$1) ; just pass the table as arg
+        (string) : `(string ,$1))
 
-  (funcbody (params block end) : $x)
+  (function-stmt (function funcname funcbody) : `(function ,$2 ,$3)
+                 (function funcbody) : `(function ,$2))
+
+  (funcbody (params block end) : `(funcbody ,$1 ,$2))
   
-  (params (lparen par-list rparen) : $x)
+  (params (lparen par-list rparen) : $2)
 
-  (par-list () : $x
-            (name-list) : $x
-            (tri-dots) : $x
-            (name-list comma tri-dots) : $x)
+  (par-list () : '(void-par-list)
+            (name-list) : $1
+            (tri-dots) : '(void-varlist) ; WTF?!
+            (name-list comma tri-dots) : '(varlist ,$1 ...))
 
-  (table-constructor (lbrace rbrace) : $x
-                     (lbrace field-list rbrace) : $x
-                     (lbrace field-list comma rbrace) : $x
-                     (lbrace field-list semi-colon rbrace) : $x)
+  (table-constructor (lbrace rbrace) : '(table)
+                     (lbrace field-list rbrace) : `(table ,$2)
+                     (lbrace field-list comma rbrace) : `(table ,$2)
+                     (lbrace field-list semi-colon rbrace) : `(table ,$2))
 
   (field-list (field) : $1
-              (field-list comma field) : $x
-              (field-list semi-colon field) : $x)
+              (field-list comma field) : `(field-list ,@$1 ,$3)
+              (field-list semi-colon field) : `(field-list ,@$1 ,$3))
 
-  (field (exp) : $x
-         (name eq exp) : $x
-         (lbracket exp rbracket eq exp) : $x)) 
+  ;; NOTE: is dot-name the same with table-bracket-ref
+  (field (exp) : $1
+         (name assign exp) : `(field-assign ,$1 ,$3)
+         (lbracket exp rbracket assign exp) : `(field-bracket-assign ,$2 ,$5))
