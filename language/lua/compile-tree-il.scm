38751d2dafccba6038e04714cec7f9e8d4a79e93
Nala Ginrut <nalaginrut@gmail.com>
2014-05-30 19:20:42 +0800

    now compile-tree-il works

--- language/lua/compile-tree-il.scm
+++ language/lua/compile-tree-il.scm
@@ -1,4 +1,4 @@
-;;  Copyright (C) 2013
+;;  Copyright (C) 2013,2014
 ;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
 ;;  This file is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
@@ -16,17 +16,11 @@
 (define-module (language lua compile-tree-il)
   #:use-module (language lua utils)
   #:use-module (language lua parser)
+  #:use-module (language lua scope)
+  #:use-module (language lua impl)
   #:use-module (language tree-il)
+  #:use-module (ice-9 match)
   #:export (compile-tree-il))
-
-(define empty-lexical-environment
-  (make-hash-table))
-
-(define-syntax-rule (@implv sym)
-  (-> (@ '(language lua impl) 'sym)))
-
-(define-syntax-rule (@impl sym arg ...)
-  (-> (%> (@implv sym) arg ...)))
 
 (define (lua-init)
   #t) ;; nothing to do yet.
@@ -35,13 +29,13 @@
   (values
    (parse-tree-il
     (begin (lua-init)
-           (comp exp empty-lexical-environment)))
+           (comp exp (current-top-level-environment))))
    env
    env))
 
 (define (lookup name env)
   (hash-ref env name))
-      
+
 (define (store name value env)
   (hash-set! env name value))
 
@@ -55,43 +49,69 @@
                (-> (begin (proc v)
                           (-> (lexical v v)))))))
 ;; for emacs:
-;; (put 'pmatch/source 'scheme-indent-function 1)
+;; (put 'match 'scheme-indent-function 1)
 
 (define (comp src e)
-  (pmatch/source src
-    (nil
-     (-> (const 'nil)))
-    (true
-     (-> (const 'true)))
-    (false
-     (-> (const 'false)))
-    ((number ,x)
+  ;;(display src)(newline)
+  (match src
+    ;; Literals
+    ('(marker nil)
+     '(const nil))
+    ('(boolean true)
+     '(const true))
+    ('(boolean false)
+     '(const false))
+    (`(number ,x)
      (-> (const x)))
-    ((string ,x)
+    (`(string ,x)
      (-> (const x)))
-    ((variable ,x)
+
+    ;; variables
+    (`(variable ,x)
      (%> lookup x e))
-    ((store ,x ,v)
+    (`(store ,x ,v)
      (%> store x (comp v e) e))
+
+    ;; scope and statment
     ;; FIXME: we need lexical scope
-    ((begin ,form)
+    (('scope rest)
+     ;;(display rest)(newline)
+     (comp rest (new-scope e)))
+    (`(begin ,form)
      (comp form e))
-    ((begin . ,forms)
+    (`(begin . ,forms)
      `(begin ,@(map (lambda (x) (comp x e)) forms)))
-    ((add ,x ,y)
-     (@impl lua-add (comp x e) (comp y e)))
-    ((minu ,x ,y)
-     (@impl lua-minus (comp x e) (comp y e)))
-    ((multi ,x ,y)
-     (@impl lua-multi (comp x e) (comp y e)))
-    ((div ,x ,y)
-     (@impl lua-div (comp x e) (comp y e)))
-    ((mod ,x ,y)
-     (@impl lua-mod (comp x e) (comp y e)))
-    ((expt ,x ,y)
-     (@impl lua-expt (comp x e) (comp y e)))
-    ((func-call ,prefix-exp ,name ,args)
-     (@impl (comp prefix-exp e) (comp name e) (comp args e)))
+
+    ;; arithmatic op
+    (`(add ,x ,y)
+     (lua-add (comp x e) (comp y e)))
+    (`(minu ,x ,y)
+     (lua-minus (comp x e) (comp y e)))
+    (`(multi ,x ,y)
+     (lua-multi (comp x e) (comp y e)))
+    (`(div ,x ,y)
+     (lua-div (comp x e) (comp y e)))
+    (`(mod ,x ,y)
+     (lua-mod (comp x e) (comp y e)))
+    (`(expt ,x ,y)
+     (@impl 'lua-expt (comp x e) (comp y e)))
+
+    ;; logical op
+    (`(lt ,x ,y)
+     (lua-lt (comp x e) (comp y e)))
+    (`(gt ,x ,y)
+     (lua-gt (comp x e) (comp y e)))
+    (`(eq ,x ,y)
+     (lua-eq (comp x e) (comp y e)))
+    (`(geq ,x ,y)
+     (lua-geq (comp x e) (comp y e)))
+    (`(leq ,x ,y)
+     (lua-lt (comp x e) (comp y e)))
+
+    ;; functions
+    (`(func-call (id ,func) (args ,args))
+     ;; FIXME: Should detect if it's lib function
+     ;;(display func)(newline)
+     (@impl (->lua func) (comp args e)))
     ;; TODO: finish the rest
-    ))
-    
+    (else (error comp "invalid src" src))))
