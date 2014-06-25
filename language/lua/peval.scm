5394b419b6947f67c4ce67e4a8302706cc13f463
Nala Ginrut <nalaginrut@gmail.com>
2014-06-25 23:50:47 +0800

    peval works

--- language/lua/peval.scm
+++ language/lua/peval.scm
@@ -25,9 +25,6 @@
   #:use-module (ice-9 match)
   #:export (peval))
 
-;; TODO
-(define (@eval . args) #t)
-
 (define *peval-arith-op*
   `((arith-add . ,+)
     (arith-minus . ,-)
@@ -39,24 +36,42 @@
 (define (get-arith-op op)
   (assq-ref *peval-arith-op* op))
 
+(define *peval-string-op*
+  `((str-concat . ,string-append)
+    ;; NOTE: # in Lua can operate either string or table.
+    ;;       So it's the duty of type-inference to confirm if
+    ;;       it's str-hash or tbl-hash.
+    (str-hash . ,string-length)))
+
+(define (get-string-op op)
+  (assq-ref *peval-string-op* op))
+
 ;; NOTE:
 ;; We have to do type checking in peval, because some node can't be fixed in
 ;; compile time. The purpose of partial-evaluator is to evaluate the node in
 ;; compile time as possible, but leave it alone when encountered the impossible
 ;; one.
 (define (peval pattern)
+  (define (try-to-reduce-op op x y)
+    ;; NOTE: all the ops are primitives
+    (let ((xx (peval x))
+          (yy (peval y)))
+      (if (and (is-immediate-object? xx) (is-immediate-object? yy))
+          ;; NOTE: Won't compile it to tree-il here, should be reduced here.
+          (op (<lua-type>-value xx) (<lua-type>-value yy)) 
+          ;;`(call (lexical ,op ,op) (const ,(<lua-type>-value xx)) (const ,(<lua-type>-value yy)))
+          ;; Can't be reduced, return the similar pattern.
+          ;; NOTE: xx and yy may be evaluated, so we should return them rather than the orginal one.
+          (list op xx yy))))
   (match pattern
+    ((and (? <lua-type>?) (? is-immediate-object?))
+     ;; For immediate type, return it directly.
+     pattern)
     (((? get-arith-op op) x y)
-     ;; NOTE: all the arith-ops are primitives
-     (let ((xx (peval x))
-           (yy (peval y)))
-       (if (and (is-immediate-object? xx) (is-immediate-object? yy))
-           ;; FIXME: don't compile it to tree-il here, should call @eval to reduce it.
-           ((@eval op) (<lua-type>-value xx) (<lua-type>-value yy)) 
-           ;;`(call (lexical ,op ,op) (const ,(<lua-type>-value xx)) (const ,(<lua-type>-value yy)))
-           ;; Can't be reduced, return the similar pattern.
-           ;; NOTE: xx and yy may be evaluated, so we should return them rather than the orginal one.
-           (list op xx yy))))
+     (try-to-reduce-op (get-arith-op op) x y))
     ;; TODO
     ;; Nothing to be reduced, return the original pattern.
-    (else pattern)))
+    (((? get-string-op op) x y)
+     (try-to-reduce-op (get-string-op op) x y))
+     ;; TODO
+    (else "no")))
