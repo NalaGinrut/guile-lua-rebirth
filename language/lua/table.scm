686514399270c171fdb061bf746b5b8bd78e0a1e
Nala Ginrut <nalaginrut@gmail.com>
2015-11-13 04:45:40 +0800

    implemented tables

--- language/lua/table.scm
+++ language/lua/table.scm
@@ -1,4 +1,4 @@
-;;  Copyright (C) 2014
+;;  Copyright (C) 2014,2015
 ;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
 ;;  This file is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 
 (define-module (language lua table)
   #:use-module (language lua utils)
-  #:use-module (srfi srfi-1)
+  #:use-module (language lua types)
   #:use-module (srfi srfi-9)
   #:use-module (srfi srfi-43)
   #:use-module (srfi srfi-69)
@@ -34,43 +34,70 @@
   (array-max lua-table-array-max lua-table-array-max!))
 
 (define (make-lua-table)
-  (%make-lua-table (make-hash-table) #f 0))
+  (%make-lua-table #f #f 0))
 
 (define (create-array-part size)
-  (if (zero? size)
-      #f
-      (make-array #f size)))
+  (make-array #f (if (zero? size) 1 size)))
 
-(define (resize-array-part! t k v)
-  (let ((new-ap (create-array-part (lua-table-array-max t)))
-        (new-hp (make-hash-table)))
-    (hash-for-each
-     (lambda (k v)
-       (if (integer? k)
-           (array-set! new-ap v k)
-           (hash-set! new-hp k v))))
-    (lua-table-array-max! t 0)
-    (lua-table-array-part! t new-ap)
-    (lua-table-hash-part! t new-hp)))     
+(define (resize-array-part! t i)
+  (let ((new-ap (create-array-part i))
+        (ap (lua-table-array-part t)))
+    (array-copy! ap new-ap)
+    (lua-table-array-max! t i)
+    (lua-table-array-part! t new-ap)))
 
-(define (try-to-set! t k v)
+(define (try-array-set! t i v)
   (let ((ap (lua-table-array-part t)))
-    (hash-set! ap k v)
-    (lua-table-array-max! t (max k (lua-table-array-max t)))))
+    (array-set! ap v i)
+    (lua-table-array-max! t (max i (lua-table-array-max t)))))
 
-(define (array-should-be-resized? t k)
-  (let ((hp (lua-table-hash-part t))
-        (n (lua-table-array-max t)))
+(define (array-should-be-resized? t)
+  (let* ((ap (lua-array-hash-part t))
+         (size (lua-table-array-max t))
+         (half (div size 2)))
     ;; all the numbers of 1 ~ (n/2) are in the slots
-    (and (every (lambda (i) (hash-ref hp i)) (iota (div n 2)))
-         ;; any number of (n/2)+1 ~ n is in the slots
-         (any (lambda (i) (hash-ref hp i)) (range (1+ (div n 2)) n)))))
+    ;; any number of (n/2)+1 ~ n is in the slots
+    (let lp((i 0) (overhalf? #f))
+      (cond
+       ((> i size) #t) ; slots are full
+       ((array-ref ap i) ; number is in the slots
+        (if overhalf? ; now it's in the upper of slots
+            #t ; at least one number of (n/2)+1 ~ n in the slots
+            (lp (1+ i) (> (1+ i) half)))) ; try next number
+       (else
+        ;; number is NOT in the slots
+        (if overhalf? ; now it's in the upper of slots
+            #f ; not all numbers of 1 ~ (n/2) are in the slots
+            (lp (1+ i) (> (1+ i) half)))))))) ; try next number
+
+(define (try-hashtable-set! t k v)
+  (let ((tp (lua-table-hash-part t)))
+    (when (not tp) (lua-table-hash-part! t (make-hash-table)))
+    (hash-table-set! (lua-table-hash-part t k v))))
 
 (define (lua-table-set! t k v)
   (cond
    ((integer? k)
+    (when (and (>= k (lua-table-array-max t)) (array-should-be-resized? t))
+          ;; k = array-size is necessary, since we need to allocate array
+          ;; when k=0 and array is null.
+          ;; NOTE: we're little different from Lua-5.0, the first index of
+          ;;       array in guile-lua-rebirth is 0, not 1.
+          (resize-array-part! t k))
     (cond
-     ((lua-table-array-part t) (try-to-set! t k v))
-     ((array-should-be-resized? t) (resize-array-part! t k v))
-     (else (hash-table-set! (lua-table-hash-part t) k v))))
-  (else (hash-table-set! (lua-table-hash-part t) k v))))
+     ;; integer k < array-size will store in the array
+     ((lua-table-array-part t) (try-array-set! t k v))
+     ;; integer k >= array-size will store in the hashtable
+     (else (try-hashtable-set! t k v))))
+   ;; non-integer k will store in the hashtable
+   (else (try-hashtable-set! t k v))))
+
+;; if no value in table, then return nil
+(define (lua-table-ref t k)
+  (or
+   (cond
+    ((integer? k)
+     (or (array-ref (lua-table-array-part t) k)
+         (hash-ref (lua-table-hash-part t) k)))
+    (else (hash-ref (lua-table-hash-part t) k)))
+   (gen-nil)))
